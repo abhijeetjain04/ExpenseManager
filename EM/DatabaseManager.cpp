0a8eea5f305b75a3225ae80574b2e555c75123d9
Aditya Bhende <bhendeaditya@gmail.com>
2023-03-04 14:22:16 +0530

    Added Singleton class DatabaseManager that will handle all DB operations.

---
+++ EM/DatabaseManager.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "DatabaseManager.h"
+
+// private
+DatabaseManager* DatabaseManager::s_Instance = nullptr;
+bool DatabaseManager::s_IsInitialized = false;
+std::mutex DatabaseManager::s_Mutex;
+
+// public
+DatabaseManager* DatabaseManager::GetInstance()
+{
+	if (!s_IsInitialized)
+		assert(!"DatabaseManager Not Initialized yet!");
+	return s_Instance;
+}
+
+// public
+DatabaseManager::DatabaseManager(const char* dbName, int openMode)
+{
+	m_Database = std::make_unique<db::Database_SQLite>(dbName, openMode);
+}
+
+// public
+void DatabaseManager::Create(const char* dbName, int openMode)
+{
+	if (s_Instance == nullptr)
+	{
+		s_Mutex.lock();
+
+		if (s_Instance == nullptr)
+		{
+			s_Instance = new DatabaseManager(dbName, openMode);
+			s_IsInitialized = true;
+		}
+
+		s_Mutex.unlock();
+	}
+}
+
+
---
+++ EM/DatabaseManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "DBHandler/Database_SQLite.h"
+
+#define databaseMgr DatabaseManager::GetInstance()
+
+class DatabaseManager
+{
+public:
+
+	static void Create(const char* dbName, int openMode = db::OPEN_CREATE | db::OPEN_READWRITE);
+
+	template<typename T>
+	std::shared_ptr<T> GetTable() const
+	{
+		return m_Database->CreateTable<T>();
+	}
+
+	static DatabaseManager* GetInstance();
+
+private:
+	DatabaseManager(const char* dbName, int openMode);
+	DatabaseManager(const DatabaseManager&) = default;
+	DatabaseManager(DatabaseManager&&) = default;
+
+	std::unique_ptr<db::Database_SQLite> m_Database;
+
+	static std::mutex s_Mutex;
+	static DatabaseManager* s_Instance;
+	static bool s_IsInitialized;
+
+
+};
+
